b6bd18c9fb31e1e221cd1ae8e81ff6682609bda2
GeoDragon <geolangsat@gmail.com>
2016-01-28 14:25:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/geonarz/myapplication/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.geonarz.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void spotifyStreamer(View view){
+        Toast.makeText(MainActivity.this, "You have selected the Spotify Streamer App", Toast.LENGTH_SHORT).show();
+    }
+
+    public void scoresApp(View view){
+        Toast.makeText(MainActivity.this, "You have selected the Scores App", Toast.LENGTH_SHORT).show();
+    }
+
+    public void libraryApp(View view){
+        Toast.makeText(MainActivity.this, "You have selected the Library App", Toast.LENGTH_SHORT).show();
+    }
+
+    public void buildItBigger(View view){
+        Toast.makeText(MainActivity.this, "You have selected the Build It Bigger App", Toast.LENGTH_SHORT).show();
+    }
+
+    public void baconReader(View view){
+        Toast.makeText(MainActivity.this, "You have selected the Bacon Reader App", Toast.LENGTH_SHORT).show();
+    }
+
+    public void capstoneApp(View view){
+        Toast.makeText(MainActivity.this, "You have selected the Capstone App", Toast.LENGTH_SHORT).show();
+    }
+}
